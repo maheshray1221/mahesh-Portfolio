9059264248715d0b11090667817a2097ad6d7dc9
maheshray1221 <mr5832388@gmail.com>
2026-02-01 14:15:37 +0530

    restyle project section

--- src/User-Pages/Project.jsx
+++ src/User-Pages/Project.jsx
@@ -1,10 +1,13 @@
+import { Projects } from "../data/Projects";
 import ProjectCard from "./ProjectCard";
 
 export default function Project() {
   return (
     <>
       <div className="w-full mt-10 text-center lg:mt-7">
-        <h1 className="text-3xl font-semibold lg:text-7xl text-[#fcffff]">Projects</h1>
+        <h1 className="text-3xl font-semibold lg:text-7xl text-[#fcffff]">
+          Projects
+        </h1>
         <div className="flex items-center lg:space-x-6 lg:ml-[50%] lg:mt-15">
           <div className="gap-1.5 relative flex ml-[50%] lg:ml-0 flex-col lg:items-center lg:gap-1.5">
             <div className="mt-3 lg:mt-0 w-1 h-10 lg:w-1 lg:h-17 bg-[#fb715e] "></div>
@@ -14,7 +17,9 @@ export default function Project() {
       </div>
       <div className="flex flex-col items-center w-full h-full lg:flex lg:flex-row lg:flex-wrap lg:min-h-screen lg:justify-around lg:w-300 lg:mt-7 lg:ml-40 ">
         {/* loop yha se start hoga */}
-        <ProjectCard/>
+        {Projects.map((project) => (
+          <ProjectCard key={project.id} {...project}/>
+        ))}
       </div>
     </>
   );
---
+++ src/data/Projects.js
@@ -0,0 +1,20 @@
+export const Projects = [
+    {
+        id: 1,
+        title: "ProTask",
+        description: "A full-stack task management application built with the MERN stack, featuring CRUD operations, RESTful APIs, and a scalable MongoDB backend. Designed with secure server-side logic and MVC architecture to ensure performance and maintainability.",
+        tech: ["React.js", "javaScript", "Node.js", "MongoDB", "mongoose", "Tailwind CSS",],
+        video: "/task.mp4",
+        live: "https://task-manager-frontend-th9q.onrender.com",
+        github: "https://github.com/maheshray1221/Task-Manager"
+    },
+    {
+        id: 1,
+        title: "Wonderlust",
+        description: "MERN stack task management application",
+        tech: ["React", "Node", "MongoDB"],
+        image: "/images/task.png",
+        live: "https://task-manager-frontend-th9q.onrender.com",
+        github: "https://github.com/maheshray1221/Task-Manager"
+    },
+]
