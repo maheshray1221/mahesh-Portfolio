56d805f2477ca18f95ddcf7bd18712f59d6691c6
maheshray1221 <mr5832388@gmail.com>
2026-02-01 11:17:12 +0530

    complete image style

--- src/User-Pages/Home.jsx
+++ src/User-Pages/Home.jsx
@@ -1,8 +1,8 @@
-import  {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-  const navigate = useNavigate("")
-  
+  const navigate = useNavigate("");
+
   return (
     <div className=" w-full flex flex-col items-center min-h-screen lg:justify-between lg:flex lg:flex-row ">
       <div className="w-full pt-6  order-2 lg:pt-15 lg:order-1 lg:w-1/2 ">
@@ -19,7 +19,8 @@ export default function Home() {
           Fullstack Developer
         </h1>
         <div className="mt-10 ml-6 lg:mt-0 lg:ml-0">
-          <a onClick={()=> navigate("/talkme")}
+          <a
+            onClick={() => navigate("/talkme")}
             href="#contact"
             className=" px-9.5 py-3 text-lg rounded-lg
           lg:relative lg:top-15 lg:text-2xl bg-[#ff715b] text-[#fcffff] lg:px-6 lg:py-3 
@@ -29,8 +30,8 @@ export default function Home() {
             Hire Me
           </a>
           <a
-          href="/Maheshray-full-stack-resume.pdf"
-          download
+            href="/Maheshray-full-stack-resume.pdf"
+            download
             className="px-5.5 py-3 text-lg rounded-lg ml-7
            lg:relative lg:top-15 lg:text-2xl border border-[#ff715b]
                    text-[#ff715b] lg:px-5 lg:py-3 lg:ml-9 lg:dark:md:hover:bg-[#ff715b] 
@@ -42,10 +43,11 @@ export default function Home() {
       </div>
       <div
         className="flex items-center overflow-hidden justify-center w-65 h-65 rounded-full border-4 mt-28 mr-0 border-orange-500  lg:border-none order-1 
-      lg:ml-0 lg:mr-30 lg:order-2 pt-5 lg:pt-10  lg:w-100 lg:h-100 lg:rounded-sm lg:mt-20 lg:flex lg:items-center lg:justify-center lg:relative"
+      lg:ml-0 lg:mr-30 lg:order-2 pt-5 lg:pt-10  lg:w-100 lg:h-100 lg:rounded-sm lg:mt-20 lg:flex lg:items-center lg:justify-center lg:relative
+      shadow-[0_0_8px_#fff,inset_0_0_8px_#fff,0_0_16px_#ff715b,inset_0_0_16px_#ff715b,0_0_28px_#ff715b,inset_0_0_28px_#ff715b]"
       >
         <img
-          className="w-full h-full rounded-full object-cover  lg:rounded-2xl "
+          className="w-full h-full rounded-full object-cover scale-106 mr-2 mb-6 lg:mr-10 lg:mb-18  lg:scale-[1.25] lg:rounded-none "
           src="/mahesh.jpg"
           alt="hero"
         />
